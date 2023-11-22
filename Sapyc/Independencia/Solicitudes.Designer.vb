3381405c727cc7c4cfcac3225fbd17ab22d58759
Octavio Alfonso Cervantes Ruiz <Octavio.A.Cervantes@mx.gt.com>
2023-11-22 17:47:29 -0600

    Cambio diseño Clientes Nuevos

--- Sapyc/Independencia/Solicitudes.Designer.vb
+++ Sapyc/Independencia/Solicitudes.Designer.vb
@@ -22,10 +22,13 @@ Partial Class Solicitudes
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.Lista = New System.Windows.Forms.DataGridView()
         Me.Bguardar = New System.Windows.Forms.Button()
         Me.BSalir = New System.Windows.Forms.Button()
+        Me.panPrincipal = New System.Windows.Forms.Panel()
         CType(Me.Lista, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.panPrincipal.SuspendLayout()
         Me.SuspendLayout()
         '
         'Lista
@@ -33,22 +36,31 @@ Partial Class Solicitudes
         Me.Lista.AllowUserToAddRows = False
         Me.Lista.AllowUserToDeleteRows = False
         Me.Lista.AllowUserToOrderColumns = True
-        Me.Lista.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Lista.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Lista.BackgroundColor = System.Drawing.Color.WhiteSmoke
         Me.Lista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.Lista.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter
-        Me.Lista.Location = New System.Drawing.Point(3, 2)
+        Me.Lista.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
+        Me.Lista.GridColor = System.Drawing.Color.Gainsboro
+        Me.Lista.Location = New System.Drawing.Point(11, 11)
         Me.Lista.Name = "Lista"
+        Me.Lista.RowHeadersWidth = 25
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(79, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(127, Byte), Integer))
+        Me.Lista.RowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.Lista.RowTemplate.Height = 26
         Me.Lista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect
-        Me.Lista.Size = New System.Drawing.Size(1005, 466)
+        Me.Lista.Size = New System.Drawing.Size(987, 543)
         Me.Lista.TabIndex = 53
         '
         'Bguardar
         '
-        Me.Bguardar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Bguardar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Bguardar.Location = New System.Drawing.Point(3, 477)
+        Me.Bguardar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Bguardar.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bguardar.Location = New System.Drawing.Point(12, 576)
         Me.Bguardar.Name = "Bguardar"
-        Me.Bguardar.Size = New System.Drawing.Size(90, 25)
+        Me.Bguardar.Size = New System.Drawing.Size(130, 25)
         Me.Bguardar.TabIndex = 56
         Me.Bguardar.Text = "&Ver"
         Me.Bguardar.UseVisualStyleBackColor = True
@@ -56,28 +68,43 @@ Partial Class Solicitudes
         'BSalir
         '
         Me.BSalir.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.BSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.BSalir.Location = New System.Drawing.Point(921, 477)
+        Me.BSalir.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BSalir.Location = New System.Drawing.Point(872, 576)
         Me.BSalir.Name = "BSalir"
-        Me.BSalir.Size = New System.Drawing.Size(87, 25)
+        Me.BSalir.Size = New System.Drawing.Size(130, 25)
         Me.BSalir.TabIndex = 54
         Me.BSalir.Text = "Salir"
         Me.BSalir.UseVisualStyleBackColor = True
         '
+        'panPrincipal
+        '
+        Me.panPrincipal.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.panPrincipal.BackColor = System.Drawing.Color.White
+        Me.panPrincipal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.panPrincipal.Controls.Add(Me.Lista)
+        Me.panPrincipal.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.panPrincipal.Location = New System.Drawing.Point(0, 0)
+        Me.panPrincipal.Name = "panPrincipal"
+        Me.panPrincipal.Size = New System.Drawing.Size(1011, 570)
+        Me.panPrincipal.TabIndex = 57
+        '
         'Solicitudes
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1011, 511)
-        Me.ControlBox = False
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(1011, 606)
+        Me.Controls.Add(Me.panPrincipal)
         Me.Controls.Add(Me.Bguardar)
         Me.Controls.Add(Me.BSalir)
-        Me.Controls.Add(Me.Lista)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
         Me.Name = "Solicitudes"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "SOLICITUDES PROPUESTA"
+        Me.Text = "Solicitudes de Propuestas"
         CType(Me.Lista, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.panPrincipal.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -85,4 +112,5 @@ Partial Class Solicitudes
     Friend WithEvents Lista As DataGridView
     Friend WithEvents Bguardar As Button
     Friend WithEvents BSalir As Button
+    Friend WithEvents panPrincipal As Panel
 End Class
--- Sapyc/Independencia/Solicitudes.vb
+++ Sapyc/Independencia/Solicitudes.vb
@@ -6,92 +6,36 @@
     Private drDat As DataRow
 
     Private Sub Solicitudes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Lista.DataSource = bsSol
+        Lista.DataSource = bsSol
+
         crearTabla()
         ListaSolicitudes()
     End Sub
+    Private Sub Bguardar_Click(sender As Object, e As EventArgs) Handles Bguardar.Click
+        Dim dlg As New frmPropuesta
+
+        Try
+            If Me.Lista.Rows.Count > 0 Then
+                If Not Me.Lista.CurrentRow Is Nothing Then
+
+                    dlg.IdProp = Lista.CurrentRow.Cells("FOLIO").Value
+                    'dlg.TpCon = "I"
+                    If dlg.ShowDialog = Windows.Forms.DialogResult.OK Then
+                        Me.Lista.DataSource = bsSol
+                        crearTabla()
+                        ListaSolicitudes()
+                    End If
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error")
+        End Try
+    End Sub
+
     Private Sub BSalir_Click(sender As Object, e As EventArgs) Handles BSalir.Click
         HabilitaMenu()
         Me.Hide()
         Me.Dispose()
-    End Sub
-    Private Sub ListaSolicitudes()
-        Try
-            With ds.Tables
-                With clsLocal
-                    .subClearParameters()
-                    .subAddParameter("@iOpcion", 5, SqlDbType.Int, ParameterDirection.Input)
-
-                End With
-
-                If .Contains("paIndependencia") Then
-                    .Remove("paIndependencia")
-                End If
-
-                .Add(clsLocal.funExecuteSPDataTable("paIndependencia"))
-                dtSolicitudes = .Item("paIndependencia")
-
-                If dtSolicitudes.Rows.Count > 0 Then
-
-                    For Each dr As DataRow In dtSolicitudes.Rows
-
-                        drDat = DtDatos.NewRow
-                        drDat("FOLIO") = dr("IDPROP").ToString()
-                        drDat("NOMCONTINICIAL") = dr("NOMEMPRESA").ToString()
-                        drDat("OFICINA") = dr("OFICINA").ToString()
-                        drDat("DIVISION") = dr("DIVISION").ToString()
-                        'drDat("SOCIO") = dr("SOCIO").ToString()
-                        drDat("REGISTRADAEL") = CDate(dr("REGISTRADAEL")).ToShortDateString()
-
-                        drDat("FECHSOLIND") = CDate(dr("FECHSOLIND")).ToShortDateString()
-                        drDat("ESTATUS") = dr("ESTATUS").ToString()
-                        DtDatos.Rows.InsertAt(drDat, DtDatos.Rows.Count)
-
-                    Next
-
-                    bsSol.DataSource = DtDatos
-                    formatoGrid()
-                End If
-
-
-            End With
-        Catch ex As Exception
-            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error")
-        End Try
-    End Sub
-    Private Sub formatoGrid()
-        Try
-            For Each col As DataGridViewColumn In Me.Lista.Columns
-                col.SortMode = DataGridViewColumnSortMode.NotSortable
-            Next
-
-            Me.Lista.Columns("FOLIO").Visible = False
-
-            Me.Lista.Columns("NOMCONTINICIAL").HeaderText = "CONTACTO INICIAL"
-            Me.Lista.Columns("NOMCONTINICIAL").Width = 250
-
-            Me.Lista.Columns("OFICINA").HeaderText = "OFICINA"
-            Me.Lista.Columns("OFICINA").Width = 100
-
-            Me.Lista.Columns("DIVISION").HeaderText = "DIVISION"
-            Me.Lista.Columns("DIVISION").Width = 100
-
-            'Me.Lista.Columns("SOCIO").HeaderText = "SOCIO"
-            'Me.Lista.Columns("SOCIO").AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
-
-            Me.Lista.Columns("ESTATUS").HeaderText = "ESTATUS"
-            Me.Lista.Columns("ESTATUS").AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
-
-            Me.Lista.Columns("REGISTRADAEL").HeaderText = "FECHA PROPUESTA"
-            Me.Lista.Columns("REGISTRADAEL").Width = 100
-
-
-            Me.Lista.Columns("FECHSOLIND").HeaderText = "FECHA SOLICITÓ"
-            Me.Lista.Columns("FECHSOLIND").Width = 100
-
-        Catch ex As Exception
-            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error")
-        End Try
     End Sub
     Private Sub Lista_DoubleClick(sender As Object, e As EventArgs) Handles Lista.DoubleClick
 
@@ -115,28 +59,9 @@
         End Try
 
     End Sub
-    Private Sub Bguardar_Click(sender As Object, e As EventArgs) Handles Bguardar.Click
-        Dim dlg As New frmPropuesta
 
-        Try
-            If Me.Lista.Rows.Count > 0 Then
-                If Not Me.Lista.CurrentRow Is Nothing Then
 
-                    dlg.IdProp = Lista.CurrentRow.Cells("FOLIO").Value
-                    'dlg.TpCon = "I"
-                    If dlg.ShowDialog = Windows.Forms.DialogResult.OK Then
-                        Me.Lista.DataSource = bsSol
-                        crearTabla()
-                        ListaSolicitudes()
-                    End If
-                End If
-            End If
-        Catch ex As Exception
-            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error")
-        End Try
-    End Sub
     Private Sub crearTabla()
-        DtDatos = New DataTable
         DtDatos.Columns.Add("FOLIO", GetType(System.String))
         DtDatos.Columns.Add("NOMCONTINICIAL", GetType(System.String))
         DtDatos.Columns.Add("OFICINA", GetType(System.String))
@@ -145,6 +70,78 @@
         DtDatos.Columns.Add("ESTATUS", GetType(System.String))
         DtDatos.Columns.Add("FECHSOLIND", GetType(System.String))
 
+    End Sub
+    Private Sub formatoGrid()
+        For Each col As DataGridViewColumn In Lista.Columns
+            col.SortMode = DataGridViewColumnSortMode.NotSortable
+        Next
+
+        Lista.Columns("FOLIO").Visible = False
+
+        Lista.Columns("NOMCONTINICIAL").HeaderText = "CONTACTO INICIAL"
+        Lista.Columns("NOMCONTINICIAL").AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+
+        Lista.Columns("OFICINA").HeaderText = "OFICINA"
+        Lista.Columns("OFICINA").Width = 150
+
+        Lista.Columns("DIVISION").HeaderText = "DIVISIÓN"
+        Lista.Columns("DIVISION").Width = 150
+
+        'Lista.Columns("SOCIO").HeaderText = "SOCIO"
+        'Lista.Columns("SOCIO").AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+
+        Lista.Columns("ESTATUS").HeaderText = "STATUS"
+        Lista.Columns("ESTATUS").Width = 150
+
+        Lista.Columns("REGISTRADAEL").HeaderText = "FECHA DE PROPUESTA"
+        Lista.Columns("REGISTRADAEL").Width = 100
+
+        Lista.Columns("FECHSOLIND").HeaderText = "FECHA DE SOLICITUD"
+        Lista.Columns("FECHSOLIND").Width = 100
+    End Sub
+
+    Private Sub ListaSolicitudes()
+        Try
+            Dim sTabla As String = "tbSol"
+
+            With ds.Tables
+                If .Contains(sTabla) Then
+                    .Remove(sTabla)
+                End If
+
+                With clsLocal
+                    .subClearParameters()
+                    .subAddParameter("@iOpcion", 5, SqlDbType.Int, ParameterDirection.Input)
+                End With
+
+                .Add(clsLocal.funExecuteSPDataTable("paIndependencia", sTabla))
+                dtSolicitudes = .Item(sTabla)
+
+                If dtSolicitudes.Rows.Count > 0 Then
+
+                    For Each dr As DataRow In dtSolicitudes.Rows
+
+                        drDat = DtDatos.NewRow
+                        drDat("FOLIO") = dr("IDPROP").ToString()
+                        drDat("NOMCONTINICIAL") = dr("NOMEMPRESA").ToString()
+                        drDat("OFICINA") = dr("OFICINA").ToString()
+                        drDat("DIVISION") = dr("DIVISION").ToString()
+                        'drDat("SOCIO") = dr("SOCIO").ToString()
+                        drDat("REGISTRADAEL") = CDate(dr("REGISTRADAEL")).ToShortDateString()
+
+                        drDat("FECHSOLIND") = CDate(dr("FECHSOLIND")).ToShortDateString()
+                        drDat("ESTATUS") = dr("ESTATUS").ToString()
+                        DtDatos.Rows.InsertAt(drDat, DtDatos.Rows.Count)
+
+                    Next
+
+                    bsSol.DataSource = DtDatos
+                    formatoGrid()
+                End If
+            End With
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error")
+        End Try
     End Sub
 
 End Class
