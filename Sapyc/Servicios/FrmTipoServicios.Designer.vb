8eb123cc48e9194a13e21c711a6a6bfdad166420
Octavio Alfonso Cervantes Ruiz <Octavio.A.Cervantes@mx.gt.com>
2025-07-08 11:07:11 -0600

    Creación de Tipos de Srevicio

---
+++ Sapyc/Servicios/FrmTipoServicios.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmTipoServicios
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle9 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle10 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.panPrincipal = New System.Windows.Forms.Panel()
+        Me.panLinea = New System.Windows.Forms.Panel()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.gridDatos = New System.Windows.Forms.DataGridView()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnOcultar = New System.Windows.Forms.Button()
+        Me.panPrincipal.SuspendLayout()
+        CType(Me.gridDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnNuevo.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNuevo.Location = New System.Drawing.Point(12, 609)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(130, 25)
+        Me.btnNuevo.TabIndex = 61
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'panPrincipal
+        '
+        Me.panPrincipal.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.panPrincipal.BackColor = System.Drawing.Color.White
+        Me.panPrincipal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.panPrincipal.Controls.Add(Me.panLinea)
+        Me.panPrincipal.Controls.Add(Me.lblTitulo)
+        Me.panPrincipal.Controls.Add(Me.gridDatos)
+        Me.panPrincipal.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.panPrincipal.Location = New System.Drawing.Point(0, 0)
+        Me.panPrincipal.Name = "panPrincipal"
+        Me.panPrincipal.Size = New System.Drawing.Size(1063, 601)
+        Me.panPrincipal.TabIndex = 60
+        '
+        'panLinea
+        '
+        Me.panLinea.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.panLinea.BackColor = System.Drawing.Color.FromArgb(CType(CType(79, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(127, Byte), Integer))
+        Me.panLinea.Location = New System.Drawing.Point(0, 36)
+        Me.panLinea.Name = "panLinea"
+        Me.panLinea.Size = New System.Drawing.Size(1063, 2)
+        Me.panLinea.TabIndex = 1
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.AutoSize = True
+        Me.lblTitulo.Font = New System.Drawing.Font("Calibri", 19.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(CType(CType(79, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(127, Byte), Integer))
+        Me.lblTitulo.Location = New System.Drawing.Point(2, 2)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(220, 32)
+        Me.lblTitulo.TabIndex = 0
+        Me.lblTitulo.Text = "TIPOS DE SERVICIO"
+        '
+        'gridDatos
+        '
+        Me.gridDatos.AllowUserToAddRows = False
+        Me.gridDatos.AllowUserToDeleteRows = False
+        Me.gridDatos.AllowUserToResizeRows = False
+        Me.gridDatos.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.gridDatos.BackgroundColor = System.Drawing.Color.WhiteSmoke
+        Me.gridDatos.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical
+        Me.gridDatos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
+        DataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle9.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.gridDatos.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle9
+        Me.gridDatos.ColumnHeadersHeight = 40
+        Me.gridDatos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
+        Me.gridDatos.GridColor = System.Drawing.Color.Gainsboro
+        Me.gridDatos.Location = New System.Drawing.Point(10, 49)
+        Me.gridDatos.MultiSelect = False
+        Me.gridDatos.Name = "gridDatos"
+        Me.gridDatos.RowHeadersWidth = 25
+        DataGridViewCellStyle10.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle10.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(79, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(127, Byte), Integer))
+        Me.gridDatos.RowsDefaultCellStyle = DataGridViewCellStyle10
+        Me.gridDatos.RowTemplate.Height = 24
+        Me.gridDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.gridDatos.Size = New System.Drawing.Size(1040, 537)
+        Me.gridDatos.TabIndex = 2
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSalir.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(921, 609)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(130, 25)
+        Me.btnSalir.TabIndex = 59
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnEditar.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEditar.Location = New System.Drawing.Point(148, 609)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(130, 25)
+        Me.btnEditar.TabIndex = 62
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'btnOcultar
+        '
+        Me.btnOcultar.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnOcultar.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOcultar.Location = New System.Drawing.Point(284, 609)
+        Me.btnOcultar.Name = "btnOcultar"
+        Me.btnOcultar.Size = New System.Drawing.Size(130, 25)
+        Me.btnOcultar.TabIndex = 63
+        Me.btnOcultar.Text = "Ocultar"
+        Me.btnOcultar.UseVisualStyleBackColor = True
+        '
+        'FrmTipoServicios
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1063, 642)
+        Me.Controls.Add(Me.btnOcultar)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.panPrincipal)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Name = "FrmTipoServicios"
+        Me.Text = "FrmTipoServicios"
+        Me.panPrincipal.ResumeLayout(False)
+        Me.panPrincipal.PerformLayout()
+        CType(Me.gridDatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents panPrincipal As Panel
+    Friend WithEvents panLinea As Panel
+    Friend WithEvents lblTitulo As Label
+    Friend WithEvents gridDatos As DataGridView
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnOcultar As Button
+End Class
---
+++ Sapyc/Servicios/FrmTipoServicios.vb
@@ -0,0 +1,115 @@
+﻿Public Class FrmTipoServicios
+
+    Private bs As New BindingSource
+    Private ds As New DataSet
+
+    Private sNameRpt As String = "Tipo de Servicios"
+    Private sStoredProc As String = "paTipoServicios"
+
+    Private dtInfo, dtServicios As New DataTable
+    Private drInfo As DataRow
+
+    Private idClave, idCveDivision As Integer
+
+    Private Sub FrmTipoServicios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        gridDatos.DataSource = bs
+        DesplazamientoGrid(gridDatos)
+
+        CrearTabla()
+        ListarTipoServicios()
+    End Sub
+    Private Sub BtnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+
+    End Sub
+    Private Sub BtnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+
+    End Sub
+    Private Sub BtnOcultar_Click(sender As Object, e As EventArgs) Handles btnOcultar.Click
+        If gridDatos.CurrentRow IsNot Nothing Then
+            idClave = gridDatos.CurrentRow.Cells("CVE").Value
+
+            If MsgBox("Se ocultará el servicio seleccionado. ¿Desea continuar?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "SIAT") = MsgBoxResult.Yes Then
+                OcultarServicio(idClave)
+
+                ListarTipoServicios()
+            End If
+        Else
+            MsgBox("Seleccione el servicio que desea ocultar.", MsgBoxStyle.Exclamation, "SIAT")
+        End If
+    End Sub
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Close()
+    End Sub
+
+    Private Sub CrearTabla()
+        dtInfo.Columns.Add("TIPO", GetType(String))
+        dtInfo.Columns.Add("CVE", GetType(String))
+        dtInfo.Columns.Add("CVEDIVISION", GetType(String))
+        dtInfo.Columns.Add("DIVISION", GetType(String))
+        dtInfo.Columns.Add("SERVICIO", GetType(String))
+        dtInfo.Columns.Add("VISIBLE", GetType(String))
+    End Sub
+    Private Sub FormatoGrid()
+        gridDatos.Columns("TIPO").Visible = False
+        gridDatos.Columns("CVE").Visible = False
+        gridDatos.Columns("CVEDIVISION").Visible = False
+
+        ConfigurarColumnasGrid(gridDatos, "DIVISION", "DIVISIÓN", 150, 1, False)
+        ConfigurarColumnasGrid(gridDatos, "SERVICIO", "SERVICIO", 0, 1, False)
+        ConfigurarColumnasGrid(gridDatos, "VISIBLE", "VISIBLE", 80, 3, False)
+    End Sub
+    Private Sub ListarTipoServicios()
+        Try
+            Dim sTabla As String = "tbTipoServicio"
+
+            With ds.Tables
+                LimpiarConsultaTabla(ds.Tables, sTabla)
+
+                With clsLocal
+                    .subClearParameters()
+                    .subAddParameter("@iOpcion", 1, SqlDbType.Int, ParameterDirection.Input)
+                End With
+
+                .Add(clsLocal.funExecuteSPDataTable(sStoredProc, sTabla))
+                dtServicios = .Item(sTabla)
+
+                If dtServicios.Rows.Count > 0 Then
+
+                    For Each dr As DataRow In dtServicios.Rows
+                        drInfo = dtInfo.NewRow
+                        drInfo("TIPO") = "S"
+                        drInfo("CVE") = dr("CVE").ToString()
+                        drInfo("CVEDIVISION") = dr("CVEDIVISION").ToString()
+                        drInfo("DIVISION") = dr("DIVISION").ToString()
+                        drInfo("SERVICIO") = dr("SERVICIO").ToString()
+                        drInfo("VISIBLE") = dr("VISIBLE").ToString()
+                        dtInfo.Rows.InsertAt(drInfo, dtInfo.Rows.Count)
+                    Next
+
+                    bs.DataSource = dtInfo
+                    FormatoGrid()
+                End If
+            End With
+        Catch ex As Exception
+            MsgBox("Por el momento no se posible mostrar el reporte, intente de nuevo más tarde.", MsgBoxStyle.Exclamation, "SIAT")
+        End Try
+    End Sub
+
+    Private Sub OcultarServicio(idClave As Integer)
+        Try
+            With clsDatosProp
+                .subClearParameters()
+                .subAddParameter("@iOpcion", 4, SqlDbType.Int, ParameterDirection.Input)
+                .subAddParameter("@idClave", idClave, SqlDbType.VarChar, ParameterDirection.Input)
+
+                .funExecuteSP(sStoredProc)
+            End With
+
+            'MsgBox("Se registraron los datos del acercamiento correctamente.", MsgBoxStyle.Information, "SIAT")
+        Catch ex As Exception
+            InsertarErrorLog(100, sNameRpt, ex.Message, sCveUsuario, "InsertarAcercamiento()")
+            MsgBox("Hubo un problema al registrar la información de accionistas, intente de nuevo más tarde.", MsgBoxStyle.Exclamation, "Error")
+        End Try
+    End Sub
+
+End Class
